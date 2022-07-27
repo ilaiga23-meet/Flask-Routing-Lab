7f4bbacf750b1b40b5aff60613fc71c4fc188414
ilaiga23-meet <ilai.galperin@gmail.com>
2022-07-27 13:42:35 +0300

    done

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -11,8 +11,18 @@ app = Flask(  # Create a flask app
 @app.route('/')
 def home_page():
     return render_template("home.html")
-
-
+@app.route('/product1')
+def airphones():
+    return render_template("airphones.html")
+@app.route('/product2')
+def iphone():
+    return render_template("product.html")
+@app.route('/product3')
+def barcelona_shirt():
+    return render_template("barcelona_shirt.html")
+@app.route('/product4')
+def macbook():
+    return render_template("macbook.html")
 
 # Your code should be above
 
